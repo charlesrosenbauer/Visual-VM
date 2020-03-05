501ebb339c034c3d8b45f3b5b3db33cf73f40f23
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2020-03-04 20:26:56 -0600

    Added core loop

--- src/main.c
+++ src/main.c
@@ -1,1 +1,35 @@
 #include "vm.h"
+#include "SDL/SDL.h"
+
+
+
+
+
+
+
+
+int main(){
+  SDL_Init(SDL_INIT_EVERYTHING);
+
+  SDL_Surface* screen = SDL_SetVideoMode(512, 512, 32, 0);
+
+  int cont = 1;
+  while(cont){
+
+    /*
+      * Run computer
+      * Visualize screen, RAM stack, RAM heap, etc.
+      * Eventually: cache, cache stalls
+    */
+
+
+    SDL_Event event;
+		while(SDL_PollEvent( &event )){
+			if(event.type == SDL_QUIT) cont = 0;
+		}
+
+    SDL_Flip(screen);
+    SDL_Delay(16);
+  }
+  SDL_Quit();
+}
