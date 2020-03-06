edad01865afe1b7516f7f23b43ecca1f7ea416cc
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2020-03-05 23:53:47 -0600

    Added some simple signedness to operations.

--- src/vm.c
+++ src/vm.c
@@ -17,102 +17,103 @@ int step(COMPUTER* comp, int steps){
       return 1;
     }
     instrc = comp->program[comp->ip];
-    uint64_t a, b, reta, retb, status;
+    REGISTER a, b, reta, retb;
+    uint64_t status;
 
     /*
       TODO: Handle signs a bit better
     */
     switch(instrc.opcode){
       case OP_ADD :{
-        int overflow = 0;
-        reta = __builtin_addl(a, b, &overflow);
-        status = overflow? status & 0xfffffffffffffffe : status | 1;
+        int64_t overflow = 0;
+        reta.i64 = __builtin_saddl_overflow(a.i64, b.i64, &overflow);
+        status   = overflow? status & 0xfffffffffffffffe : status | 1;
       }break;
 
       case OP_SUB :{
-        int overflow = 0;
-        reta = __builtin_subl(a, b, &overflow);
-        status = overflow? status & 0xfffffffffffffffe : status | 1;
+        int64_t overflow = 0;
+        reta.i64 = __builtin_ssubl_overflow(a.i64, b.i64, &overflow);
+        status   = overflow? status & 0xfffffffffffffffe : status | 1;
       }break;
 
       case OP_ADC :{
-        int ovf0 = 0, ovf1 = 0;
-        a    = __builtin_addl(a, comp->status & OVERFLOW, &ovf0);
-        reta = __builtin_addl(a, b, &ovf1);
-        status = (ovf0 | ovf1)? status & 0xfffffffffffffffe : status | 1;
+        int64_t ovf0 = 0, ovf1 = 0;
+        a.i64    = __builtin_saddl_overflow(a.i64, comp->status & OVERFLOW, &ovf0);
+        reta.i64 = __builtin_saddl_overflow(a.i64, b.i64, &ovf1);
+        status   = (ovf0 | ovf1)? status & 0xfffffffffffffffe : status | 1;
       }break;
 
       case OP_SBB :{
-        int ovf0 = 0, ovf1 = 0;
-        a    = __builtin_subl(a, comp->status & OVERFLOW, &ovf0);
-        reta = __builtin_addl(a, b, &ovf1);
-        status = (ovf0 | ovf1)? status & 0xfffffffffffffffe : status | 1;
+        int64_t ovf0 = 0, ovf1 = 0;
+        a.i64    = __builtin_ssubl_overflow(a.i64, comp->status & OVERFLOW, &ovf0);
+        reta.i64 = __builtin_saddl_overflow(a.i64, b.i64, &ovf1);
+        status   = (ovf0 | ovf1)? status & 0xfffffffffffffffe : status | 1;
       }break;
 
       case OP_IMUL :{
-        int overflow = 0;
-        reta = __builtin_smull(a, b, &overflow);
-        __int128 ax = a, bx = b;
+        int64_t overflow = 0;
+        __builtin_smull_overflow(a.i64, b.i64, &overflow);
+        __int128 ax = a.i64, bx = b.i64;
         ax *= bx;
-        reta = ax;
-        retb = ax >> 64;
+        reta.i64 = ax;
+        retb.i64 = ax >> 64;
         status = overflow? status & 0xfffffffffffffffe : status | 1;
       }break;
 
       case OP_UMUL :{
-        int overflow = 0;
-        reta = __builtin_umull(a, b, &overflow);
-        unsigned __int128 ax = a, bx = b;
+        uint64_t overflow = 0;
+        __builtin_umull_overflow(a.u64, b.u64, &overflow);
+        unsigned __int128 ax = a.u64, bx = b.u64;
         ax *= bx;
-        reta = ax;
-        retb = ax >> 64;
+        reta.u64 = ax;
+        retb.u64 = ax >> 64;
         status = overflow? status & 0xfffffffffffffffe : status | 1;
       }break;
 
       case OP_UDIV :{
-        if(b != 0){
-          reta = a / b;
-          retb = a % b;
+        if(b.u64 != 0){
+          reta.u64 = a.u64 / b.u64;
+          retb.u64 = a.u64 % b.u64;
         }else{
           return 2;
         }
       }break;
 
       case OP_IDIV :{
-        if(b != 0){
-          reta = a / b;
-          retb = a % b;
+        if(b.i64 != 0){
+          reta.i64 = a.i64 / b.i64;
+          retb.i64 = a.i64 % b.i64;
         }else{
           return 2;
         }
       }break;
 
       case OP_XOR :{
-        reta = a ^ b;
+        reta.u64 = a.u64 ^ b.u64;
       }break;
 
       case OP_AND :{
-        reta = a & b;
+        reta.u64 = a.u64 & b.u64;
       }break;
 
       case OP_OR  :{
-        reta = a | b;
+        reta.u64 = a.u64 | b.u64;
       }break;
 
       case OP_SHL :{
-        reta = a >> b;
+        reta.u64 = a.u64 >> b.u64;
       }break;
 
       case OP_SHR :{
-        reta = a << b;
+        reta.u64 = a.u64 << b.u64;
       }break;
 
       case OP_RTL :{
-        reta = (a >> b) | (a << (64 - b));
+        reta.u64 = (a.u64 >> b.u64) | (a.u64 << (64 - b.u64));
       }break;
 
       case OP_RTR :{
-        reta = (a << b) | (a >> (64 - b));
+        reta.u64 = (a.u64 << b.u64) | (a.u64 >> (64 - b.u64));
       }break;
 
 
--- src/vm.h
+++ src/vm.h
@@ -61,6 +61,20 @@ typedef enum{
 }OPSIZE;
 
 
+typedef union{
+  uint8_t  u8;
+  int8_t   i8;
+  uint16_t u16;
+  int16_t  i16;
+  uint32_t u32;
+  int32_t  i32;
+  uint64_t u64;
+  int64_t  i64;
+  float    f32;
+  double   f64;
+}REGISTER;
+
+
 typedef struct{
   OPCODE   opcode;
   OPSIZE   size;
@@ -73,7 +87,7 @@ typedef struct{
 typedef struct{
   void*          ram;
   INSTRUCTION*   program;
-  uint64_t       registers[16];
+  REGISTER       registers[16];
   uint64_t       status, ip;
   uint32_t*      screen;
   int            programsize, ramsize, h, w;
